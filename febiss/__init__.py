a44f222ff0e6dbd8d4f30dcd5b0b802c6c555177
maberl1 <l.magenheim@protonmail.com>
2023-12-14 12:56:41 +0100

    Changed Copyright

--- febiss/__init__.py
+++ febiss/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 __copyright__ = """
 This code is licensed under the MIT license.
-Copyright University Innsbruck, Institute for General, Inorganic, and Theoretical Chemistry, Podewitz Group
+Copyright Technische Universität Wien, Institute of Materials Chemistry, Podewitz Group
 See LICENSE for details
 """
 
